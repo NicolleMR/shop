2ac3ef836128f34ec5de3adead5e99822257a5fd
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-17 22:14:18 -0500

    install react router dom library

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,35 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import "./app.css";
 
 const App = () => (
-  <div>
-    
-  </div>
+  <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/petesita">Petesita</Link>
+          </li>
+          <li>
+            <Link to="/motito">Motito</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="petesita" element={<h1>hackico</h1>} />
+        <Route path="motito" element={<h1>nalita</h1>} />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
