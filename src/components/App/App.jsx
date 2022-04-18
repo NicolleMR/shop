823e22efa823b3a327136276aeb3fb586ce0ca92
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-17 22:53:47 -0500

    Create header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,33 +2,20 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
+import Header from "../Header/Header";
 import "./app.css";
 
 const App = () => (
   <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/petesita">Petesita</Link>
-          </li>
-          <li>
-            <Link to="/motito">Motito</Link>
-          </li>
-        </ul>
-      </nav>
+    <main>
+      <Header />
       <Routes>
         <Route path="/" element={<h1>home</h1>} />
         <Route path="petesita" element={<h1>hackico</h1>} />
         <Route path="motito" element={<h1>nalita</h1>} />
       </Routes>
-    </div>
+    </main>
   </BrowserRouter>
 );
 
